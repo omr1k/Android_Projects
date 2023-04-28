c53878a974a55a339b099f3ba7fb1c8737cc23f2
Omar Khattab <omr11kk@gmail.com>
2023-04-28 07:29:34 +0300

    Update android.java

--- android.java
+++ android.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("The listed Android apps are with Java"); 
+        System.out.println("The listed Android apps are Build with Java"); 
     }
 }
